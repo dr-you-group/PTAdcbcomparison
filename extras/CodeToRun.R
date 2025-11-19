929bb640089f8d7792c880a1567b13b0e1678f0a
Paul Han <paul9567@yuhs.ac>
2025-11-19 18:32:45 +0900

    Initial commit

---
+++ extras/CodeToRun.R
@@ -0,0 +1,62 @@
+#renv::restore
+
+library(PTAdcbcomparison)
+
+# Optional: specify where the temporary files (used by the Andromeda package) will be created:
+options(andromedaTempFolder = "C:/andromedaTemp")
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores() - 1
+
+# The folder where the study intermediate and result files will be written:
+outputFolder <- ""
+
+# Details for connecting to the server:
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sql server",
+                                                                server = "",
+                                                                user = "",
+                                                                password = "",
+                                                                port = )
+
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- ""
+
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- ""
+cohortTable <- ""
+
+# Some meta-information that will be used by the export function:
+databaseId <- ""
+databaseName <- ""
+databaseDescription <- ""
+
+# For Oracle: define a schema that can be used to emulate temp tables:
+oracleTempSchema <- NULL
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        oracleTempSchema = oracleTempSchema,
+        outputFolder = outputFolder,
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        createCohorts = TRUE,
+        synthesizePositiveControls = FALSE,
+        runAnalyses = TRUE,
+        packageResults = TRUE,
+        maxCores = maxCores)
+
+
+resultsZipFile <- file.path(outputFolder, "export", paste0("Results_", databaseId, ".zip"))
+dataFolder <- file.path(outputFolder, "shinyData")
+
+# You can inspect the results if you want:
+prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+launchEvidenceExplorer(dataFolder = dataFolder, blind = FALSE, launch.browser = FALSE)
+
+# Upload the results to the OHDSI SFTP server:
+# privateKeyFileName <- ""
+# userName <- ""
+# uploadResults(outputFolder, privateKeyFileName, userName)
